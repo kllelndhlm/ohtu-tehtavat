42836ecb3217e84fd17ccdc0e80a48d07057be84
Kalle Lindholm <kalle.lindholm@helsinki.fi>
2021-12-13 22:36:08 +0200

    tehtävä 2

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -27,9 +27,8 @@ class HasAtLeast:
         return player_value >= self._value
 
 class Not:
-    def __init__(self, value, attr):
+    def __init__(self, value):
         self._value = value
-        self._attr = attr
 
     def matches(self, player):
         player_value = getattr(player, self._attr)
