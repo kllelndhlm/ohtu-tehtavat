3a623c10092d3405fbdb7e8f0f1fc0c2e663429d
Kalle Lindholm <kalle.lindholm@helsinki.fi>
2021-12-13 23:00:02 +0200

    tehtävä 2

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -29,7 +29,7 @@ class HasAtLeast:
 class Not:
     def __init__(self, event):
         self._attr = print(event)
-
+        print(self._atrr)
     def matches(self, player):
         player_value = getattr(player, self._attr)
 
